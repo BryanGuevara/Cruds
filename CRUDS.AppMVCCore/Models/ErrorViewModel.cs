d1897cd8a04c875b5098093efbab0c119afa6fb2
Bagg 12 <bryanalbertoguevaraguerrero@gmail.com>
2023-05-13 19:12:29 -0600

    Agregar archivos de proyecto.

---
+++ CRUDS.AppMVCCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUDS.AppMVCCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
